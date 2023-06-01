e0a3b228e574fc662bd2c488d9b971cd2825ddf8
anurag-adm <anurag026mishra@gmail.com>
2023-06-01 23:50:24 +0530

    June 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,22 @@ class Diceapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        
+        body: Activity1(),
       ),
     );
+  }
+}
+
+class Activity1 extends StatefulWidget {
+  const Activity1({super.key});
+
+  @override
+  State<Activity1> createState() => _Activity1State();
+}
+
+class _Activity1State extends State<Activity1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
