94036930b4eea290f1b31c32ba6eec7fbb0feff0
anurag-adm <anurag026mishra@gmail.com>
2023-06-03 23:17:10 +0530

    3 Jun

--- lib/main.dart
+++ lib/main.dart
@@ -27,26 +27,39 @@ class _DiceStructureState extends State<DiceStructure> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(image: AssetImage('image/dice-$randNum.png')),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  randNum = randomNumber(randNum);
-                });
-              },
-              child: const Text(
-                "Press",
-                style: TextStyle(fontSize: 25),
-              ),
-            )
-          ],
-        )),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 136, 177, 219),
+              Color.fromARGB(255, 53, 88, 141),
+              Color.fromARGB(255, 45, 92, 150)
+              
+            ])
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(image: AssetImage('image/dice-$randNum.png')),
+              Card(
+                color: const Color.fromARGB(255, 171, 174, 190),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      randNum = randomNumber(randNum);
+                    });
+                  },
+                  child: const Text(
+                    "Press",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
